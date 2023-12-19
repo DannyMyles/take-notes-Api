7ec945fc560ecc0bfa0e573e03a06f893d6eb670
DannyMyles <dannymyles1999@gmail.com>
2023-12-19 23:47:46 +0300

    Fix delete user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -13,7 +13,7 @@ const createUser = asyncWrapper(async(req, res)=>{
 
     // validating data, check if the required fields are being send from the frontend
     // !Array.isArray(roles),, this checks if roles is not an array
-    if(!username || !password, !Array.isArray(roles), !roles.length){
+    if(!username || !password, !Array.isArray(roles), !roles?.length){
         return res.status(HTTP_STATUS_CODES.BAD_REQUEST).json({msg:"All fields are required!"})
     }
 
@@ -46,7 +46,7 @@ const createUser = asyncWrapper(async(req, res)=>{
 
     await newUer.save(newUer)
 
-    
+    console.log("user created", newUer)
     res.status(HTTP_STATUS_CODES.CREATED).json({user: newUer})
 })
 
@@ -67,17 +67,16 @@ const getUsers = asyncWrapper(async(req, res)=>{
 // Update user
 
 const updateUser = asyncWrapper(async (req, res) => {
-    const userId = req.params.id;
-    const { username, password, roles, active } = req.body;
+    const {id, username, password, roles, active } = req.body;
 
     // validating data, check if the required fields are being sent from the frontend
-    if (!userId || !username || !password || !Array.isArray(roles) || !roles.length || typeof active !== 'boolean') {
+    if (!id || !username || !password || !Array.isArray(roles) || !roles.length || typeof active !== 'boolean') {
         return res.status(HTTP_STATUS_CODES.BAD_REQUEST).json({ msg: "All fields are required!" });
     }
 
     try {
         // Find in the DB then ...
-        const user = await User.findById(userId);
+        const user = await User.findById(id);
 
         // ... check if he exists
         if (!user) {
@@ -88,7 +87,7 @@ const updateUser = asyncWrapper(async (req, res) => {
         const dupUser = await User.findOne({ username }).lean().exec();
 
         // Allow updates to the original user
-        if (dupUser && dupUser._id.toString() !== userId) {
+        if (dupUser && dupUser._id.toString() !== id) {
             return res.status(HTTP_STATUS_CODES.CONFLICT).json({ msg: "Duplicate username!" });
         }
 
@@ -113,31 +112,39 @@ const updateUser = asyncWrapper(async (req, res) => {
     }
 });
 
-const deleteUser = asyncWrapper(async(req,res)=>{
-    const userId = req.body
-    if(!userId){
-        return res.status(HTTP_STATUS_CODES.NOT_FOUND).json('UserID request')
+const deleteUser = asyncWrapper(async (req, res) => {
+    const userId = req.body.userId; // Use req.body.userId instead of req.body
+  
+    if (!userId) {
+      return res
+        .status(HTTP_STATUS_CODES.BAD_REQUEST)
+        .json({ msg: 'UserID is required in the request body' }); // Use BAD_REQUEST instead of NOT_FOUND
     }
-
-    // const delUser = await User.findByIdAndRemove({userId})
-    // if(!delUser){
-    //     return res.status(HTTP_STATUS_CODES.NOT_FOUND).json('Invalid request')
-    // }
-    res.status(HTTP_STATUS_CODES.NO_CONTENT).json()
-
+  
     // We do not want to delete a user if assigned to the notes
-    const notes = await Note.findOne({user: id}).lean().exec()
-    if(notes?.length){
-        return res.status(HTTP_STATUS_CODES.BAD_REQUEST).json({msg: "User has assigned notes"}) 
+    const notes = await Note.find({ user: userId }).lean().exec(); // Use userId instead of id
+    if (notes.length > 0) {
+      return res
+        .status(HTTP_STATUS_CODES.BAD_REQUEST)
+        .json({ msg: 'User has assigned notes' });
     }
-
+  
     // Define our user
-    const user = await User.findById(userId)
-    if(!user){
-        return res.status(HTTP_STATUS_CODES.NOT_FOUND).json({msg: 'User does not exist'})
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(HTTP_STATUS_CODES.NOT_FOUND).json({ msg: 'User does not exist' });
     }
-
-    const result = await User.deleteOne()
-    res.status(HTTP_STATUS_CODES.OK).json({result})
-})
+  
+    // Remove the user
+    const result = await User.findByIdAndRemove(userId); // Use findByIdAndRemove directly
+  
+    if (!result) {
+      return res
+        .status(HTTP_STATUS_CODES.NOT_FOUND)
+        .json({ msg: 'User not found or already deleted' });
+    }
+  
+    res.status(HTTP_STATUS_CODES.OK).json({ msg: 'User was deleted successfully!' });
+  });
+  
 module.exports = {createUser, getUsers, updateUser, deleteUser}
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -4,6 +4,6 @@ const router = express.Router()
 
 
 router.route('/users').get(usersController.getUsers).post(usersController.createUser)
-router.route('/user/:id').put(usersController.updateUser).delete(usersController.deleteUser)
+router.route('/user').put(usersController.updateUser).delete(usersController.deleteUser)
 
 module.exports = router
